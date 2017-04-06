63e2374e818fab3c4eac6dea77b39b21e87bc7ac
github username <github email address>
2017-04-06 17:12:18 -0400

    team: @gabystern - completed

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,45 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :title, :price, :discount, :total, :items, :prices
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @prices = []
+  end
+
+  def self.total
+    @total
+  end
+
+  def add_item(title, price, quantity=1)
+    @total += price*quantity
+    quantity.times do @items << title
+    end
+    last_transaction = price*quantity
+    @prices << last_transaction
+    @total
+  end
+
+  def apply_discount
+    if discount == 0
+      "There is no discount to apply."
+    else
+      @total *= ((100-discount)*0.01)
+      @total = @total.to_i
+      "After the discount, the total comes to $#{@total}."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total -= @prices.pop
+  end
+
+end
